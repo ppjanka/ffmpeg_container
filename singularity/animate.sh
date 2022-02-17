6b5313f829432492cef48e217b9f06399a1eae6a
Patryk Pjanka <patryk.pjanka@gmail.com>
2022-02-17 15:21:08 +0100

    build and animate scripts added for singularity.

---
+++ singularity/animate.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Use docker-ffmpeg to animate all png files from the folder given as argument
+
+# Parse the arguments
+nproc=1
+while getopts ":n:i:o:" argname; do
+    case $argname in
+        n) nproc=$OPTARG;;
+        i) input_dir=$OPTARG;;
+        o) output_name=$OPTARG;;
+        ?) echo Argument not recognized. Aborting.; exit;;
+    esac
+done
+if [[ -z $input_dir || -z $output_name ]]; then
+    echo "Please specify the input directory and output name (-i, -o). Aborting."
+    exit
+fi
+# extend to absolute paths
+input_dir=$(realpath $input_dir)
+output_name=$(realpath $output_name)
+# get the output directory for passthrough
+output_dir=$(dirname "$output_name")
+if [ "$output_dir" = '.' ]; then
+    output_dir=$(pwd)
+fi
+echo $output_dir
+
+# Process using ffmpeg
+singularity exec \
+ --bind "$input_dir":'/workdir/input' \
+ --bind "$output_dir":'/workdir/output' \
+ ffmpeg_container.sif \
+ ffmpeg -threads $nproc -y -r 20 -f image2 -i \"input/comparison_%*.png\" -f mp4 -q:v 0 -vcodec mpeg4 -r 20 output/$(basename $output_name)
---
+++ singularity/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo singularity build ffmpeg_container.sif ffmpeg_container.def
